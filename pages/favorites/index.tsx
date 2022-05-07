42582b1ab4a34c20048173c8c38c8249cc3535f0
Facundo Tanovich <facundotanovich@gmail.com>
2022-05-07 10:48:12 -0300

    Fin Seccion 4

---
+++ pages/favorites/index.tsx
@@ -0,0 +1,12 @@
+import { Layout } from "../../components/layouts"
+
+const FavoritesPages = () => {
+  return (
+    <Layout title="Mis favoritos">
+        <h1>Favorites</h1>
+    </Layout>
+  )
+}
+
+
+export default FavoritesPages;
